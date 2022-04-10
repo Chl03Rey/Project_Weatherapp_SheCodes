4a6d0b75613a21a11a367932a4a18047e37d6f98
ChloeReynaud <101611969+Chl03Rey@users.noreply.github.com>
2022-04-10 12:34:27 -0700

    Display the forecast data using the API

--- app.js
+++ app.js
@@ -21,34 +21,47 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
                 <div class="weather-forecast-date">
-                ${day}
+                ${formatDay(forecastDay.dt)}
                 </div>
-                <img src="http://openweathermap.org/img/wn/04n@2x.png" alt=""
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" alt=""
                  width="50">
                  <div class="weather-forecast-temp">
                   <span class="forecast-temp-maxi">
-                  18°
+                  ${Math.round(forecastDay.temp.max)}
                   </span>  
                    | 
                   <span class="forecast-temp-mini">
-                   12°
+                   ${Math.round(forecastDay.temp.min)}
                    </span>
                  </div>
               </div>
               `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
